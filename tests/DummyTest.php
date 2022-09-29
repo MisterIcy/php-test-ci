ed4945f4e4ca8bbe233a3caf91e9690f75a6b589
Alexandros Koutroulis <icyd3mon@gmail.com>
2022-09-29 10:30:25 +0300

    Add dummy test

---
+++ tests/DummyTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akoutroulis\PhpTestCi\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class DummyTest extends TestCase
+{
+    public function testDummy(): void
+    {
+        self::assertTrue(true);
+    }
+}
